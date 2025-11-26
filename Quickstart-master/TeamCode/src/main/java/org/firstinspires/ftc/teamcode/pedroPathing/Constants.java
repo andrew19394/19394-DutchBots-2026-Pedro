c8f200cc4b114f38c425255f47ee15d50ab14e67
andrew19394 <andrewrharp@gmail.com>
2025-11-25 20:57:35 -0600

    tuned the PIDF's for the first time

--- Quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ Quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
@@ -17,7 +19,12 @@ public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(6.7)
             .forwardZeroPowerAcceleration(-57.6112)
-            .lateralZeroPowerAcceleration(-77.7596);
+            .lateralZeroPowerAcceleration(-77.7596)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.09, 0.00007, 0.009, 0.01))
+            .headingPIDFCoefficients(new PIDFCoefficients(0.7, 0, 0, 0.025))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.05, 0, 0.0001, 0.6, 0.01))
+            .centripetalScaling(0.05)
+            ;
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(.67)
             .xVelocity(39.4962)
@@ -39,7 +46,10 @@ public class Constants {
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
             .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
             .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static PathConstraints pathConstraints = new PathConstraints(0.99,
+            100,
+            1,
+            1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
